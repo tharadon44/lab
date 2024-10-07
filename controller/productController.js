c854424d248363e629ba976164f118accd5e95e7
tharadon44 <tharadon123za@gmail.com>
2024-10-07 15:08:05 +0700

    first commit

--- controller/productController.js
+++ controller/productController.js
@@ -2,54 +2,56 @@ const express = require("express")
 
 const Product = require("../models/product")
 //GET
-exports.getProduct = async(req , res) =>{
+exports.getProductID = async (req,res) =>{
+    try{
+        const { id } = req.params;
+        const product  = await Product.findById(id);
+        if (!product) return res.status(404).json({message:"Product not found"});
+        res.status(200).json(product);
+    }catch (err){
+        res.status(500).json({ message: err.message });
+    }
+};
+exports.getProduct = async (req, res) => {
     try{
         const products = await Product.find();
         res.status(200).json(products);
-    }catch (error){
-        res.status(500).json({message:error.message});
-    }
-};
-exports.getProductID = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+
+    }catch (err){
+        res.status(500).json({message: err.message});
     }
 };
 exports.postProduct = async (req, res) => {
-    try {
-        const { product_name, product_type, price, unit } = req.body;
-        const product = new Product({ product_name, product_type, price, unit });
-        const savedProduct = await product.save();
-        res.status(201).json(savedProduct);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+    const {product_name, product_type, price, unit} = req.body;
+    const product = new Product({product_name, product_type, price,unit})
+    try{
+        const newProduct = await product.save();
+        res.status(201).json(newProduct);
+    }catch (err){
+        res.status(400).json({message: err.message});
     }
 };
 exports.updateProduct = async (req, res) => {
-    try {
-        const { id } = req.params;
+    try{
+        const {id} = req.params;
         const product = await Product.findById(id);
-        if (!product) return res.status(404).json({ message: 'Product not found' });
-        const update = req.body;
-        Object.assign(product, update);
-        const updatedProduct = await product.save();
-        res.json(updatedProduct);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+        if(!product) return res.status(404).json({message : 'Product not found'});
+        const data = {$set : req.body};
+        await Product.findByIdAndUpdate(id,data);
+        res.status(200).json({ message: 'Product updated successfully' });
+    }catch (err) { 
+        res.status(400).json({ message: err.message }); 
     }
-};
+}
+
 exports.deleteProduct = async (req, res) => {
-    try {
+    try{
         const { id } = req.params;
         const product = await Product.findById(id);
-        if (!product) return res.status(404).json({ message: 'Product not found' });
+        if(!product) return res.status(404).json({message: 'Product not found'});
         await Product.findByIdAndDelete(id);
-        res.json({ message: 'Product deleted' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+        res.status(200).json({ message: 'Product deleted successfully' });
+    }catch(err){
+        res.status(500).json({message : err.message});
     }
-};
+}
